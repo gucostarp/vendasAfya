1440187db7b211278ad4f934a037a7c451a6ab59
Gustavo Costa <gucostarp@gmail.com>
2021-05-27 22:33:24 -0300

    Dados

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+router.get('/store', function(req, res, next) {
+    res.render('store', { title: 'Express' });
+});
+router.get('/products', function(req, res, next) {
+    res.render('products', { title: 'Express' });
+});
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'Express' });
+});
+
+
+module.exports = router;
